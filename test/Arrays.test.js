29ca625debc3a992a918cdb8218123c13439f868
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-25 18:16:48 -0700

    filter test done

--- test/Arrays.test.js
+++ test/Arrays.test.js
@@ -1,9 +1,10 @@
 const assert = require('assert');
 const { map } = require('../lib/arrays');
+const { filter } = require('../lib/arrays');
 
 
 describe('array methods', () => {
-
+    // map array
     describe('assumptions', () => {
         it('maps an array', () => {
             const numbers = [1, 2, 3]; 
@@ -16,6 +17,14 @@ describe('array methods', () => {
             const numbers = [1, 2, 3]; 
             const mapped = map(numbers, (n) => n + 1);
             assert.deepEqual(mapped, [2, 3, 4]);
+        });
+    });
+    // Filter arrays
+    describe('assumptions', () => {
+        it('filters an array', () => {
+            const numbers = [1, 2, 3];
+            const filtered = numbers.filter(n => n < 3);
+            assert.deepEqual(filtered, [1, 2]);
         });
     });
 });
